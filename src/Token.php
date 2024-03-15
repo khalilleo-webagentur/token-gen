322544e2a005b079e8c8d2c4e7cf1caa3bd6ee0a
ronijan <hu@hu.com>
2024-03-15 12:14:04 +0100

    code improvments

--- src/Token.php
+++ src/Token.php
@@ -10,23 +10,23 @@ final class Token
 {
     protected const LENGTH = 32;
 
-    protected const PW_LENGTH = 8;
+    protected const PW_MIN = 8;
+
+    protected const DIGITS_MIN = 6;
 
     public function getRandomToken(int $length = self::LENGTH): string
     {
-        return $this->generate(str_shuffle(CharSet::LETTERS_AND_DIGITS), $length);
+        return $this->generate(CharSet::LETTERS_AND_DIGITS, $length);
     }
 
     public function getRandomTokenOnlyLowers(int $length = self::LENGTH): string
     {
-        return $this->generate(str_shuffle(CharSet::LOWER_LETTERS), $length);
+        return $this->generate(CharSet::LOWER_LETTERS, $length);
     }
 
     public function getRandomTokenOnlyUppers(int $length = self::LENGTH): string
     {
-        $chars = str_shuffle(CharSet::UPPER_LETTERS);
-
-        return $this->generate($chars, $length);
+        return $this->generate(CharSet::UPPER_LETTERS, $length);
     }
 
     public function getRandomApiToken(int $length = self::LENGTH): string
@@ -38,7 +38,7 @@ final class Token
 
     public function getRandomDigits(int $length): int
     {
-        return (int)str_shuffle(substr(str_shuffle(CharSet::DIGITS), 0, $length));
+        return (int) $this->generate(CharSet::DIGITS, $length);
     }
 
     public function getOtp(): int
@@ -46,11 +46,11 @@ final class Token
         try {
             return random_int(111111, 999999);
         } catch (Exception $e) {
-            return (int) str_shuffle(substr(CharSet::DIGITS, 0, 6));
+            return (int) $this->generate(CharSet::DIGITS, self::DIGITS_MIN);
         }
     }
 
-    public function getRandomPassword(int $length = self::PW_LENGTH): string
+    public function getRandomPassword(int $length = self::PW_MIN): string
     {
         $lowerLetters = str_shuffle(CharSet::LOWER_LETTERS);
 
@@ -66,6 +66,6 @@ final class Token
 
     private function generate(string $chars, int $length): string
     {
-        return str_shuffle(substr($chars, 0, $length));
+        return str_shuffle(substr(str_shuffle($chars), 0, $length));
     }
 }
